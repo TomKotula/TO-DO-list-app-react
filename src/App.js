beca4e1fcbe5370571bb4edb8af5b3e47798c94a
Tom Kotula <test-git@youcode.pl>
2024-01-10 21:26:50 +0100

    toggleHideDone interaction

--- src/App.js
+++ src/App.js
@@ -6,24 +6,41 @@ import Header from "./Header";
 import Main from "./Main";
 import SectionList from "./SectionList";
 import Container from "./Container";
+import { useState } from "react";
 
 const tasks = [
   { id: 1, content: "przejsc na reacta", done: false },
   { id: 2, content: "zjesc kolacje", done: true },
 ]
 
-const hideDone = false;
-
 function App() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  };
+
   return (
     <Container>
       <Header title="TO-DO list" />
       <Main>
         <Form />
-        <Section title="TO-DO list" extraSectionContent={<Buttons tasks={tasks} hideDone={hideDone} />} />
+        <Section
+          title="TO-DO list"
+          extraSectionContent={
+            <Buttons
+              tasks={tasks}
+              hideDone={hideDone}
+              toggleHideDone={toggleHideDone}
+            />
+          }
+        />
       </Main>
       <SectionList>
-        <Tasks tasks={tasks} hideDone={hideDone} />
+        <Tasks
+          tasks={tasks}
+          hideDone={hideDone}
+        />
       </SectionList>
     </Container>
   );
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,18 +1,18 @@
 import "./style.css";
 
-const Buttons = (props) => (
-    props.tasks.length > 0 && (
-        <div className="js-toggleButtonEvents">
-            <button className="js-toggleButtonEvents js-hideCompletedTasksButton">
-                {props.hideDone ? 'Show' : 'Hide'} completed tasks
-            </button>
-            <button
-                className="js-toggleButtonEvents js-markAllTasksAsDone"
-                disabled={props.tasks.every(({ done }) => done)}
-            >
-                Complete all tasks
-            </button>
-        </div>
+const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
+    tasks.length > 0 && (
+    <div className="js-toggleButtonEvents">
+        <button onClick={toggleHideDone} className="js-toggleButtonEvents js-hideCompletedTasksButton">
+            {hideDone ? 'Show' : 'Hide'} completed tasks
+        </button>
+        <button
+            className="js-toggleButtonEvents js-markAllTasksAsDone"
+            disabled={tasks.every(({ done }) => done)}
+        >
+            Complete all tasks
+        </button>
+    </div>
     )
 );
 
