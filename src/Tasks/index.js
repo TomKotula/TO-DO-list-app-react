c92dbbf9f8a0a34433ef99f7813c2df519b7b15d
Tom Kotula <test-git@youcode.pl>
2024-01-10 21:34:07 +0100

    destructuring and ternary operator added

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -2,21 +2,23 @@ import "./style.css";
 import checkmark from './checkmark.png';
 import bin from './bin.png';
 
-const Tasks = (props) => (
+const Tasks = ({ tasks, hideDone }) => (
     <ul className="flex__list js-tasks">
-        {props.tasks.map(task => (
-            <li
-                key={task.id}
-                className="flex__listItem"
-            >
-                <button className="js-done">
-                    {task.done ? <img src={checkmark} className="js-checkmarkIcon" alt="checkmark" /> : null}
-                </button>
-                <div className={`js-content ${task.done ? 'js-contentLineThrough' : ''}`}>
-                    {task.content}
-                </div>
-                <img className="js-remove" src={bin} alt="bin"/>
-            </li>
+        {tasks.map(task => (
+            (hideDone && task.done) ? null : (
+                <li
+                    key={task.id}
+                    className="flex__listItem"
+                >
+                    <button className="js-done">
+                        {task.done ? <img src={checkmark} className="js-checkmarkIcon" alt="checkmark" /> : null}
+                    </button>
+                    <div className={`js-content ${task.done ? 'js-contentLineThrough' : ''}`}>
+                        {task.content}
+                    </div>
+                    <img className="js-remove" src={bin} alt="bin" />
+                </li>
+            )
         ))}
     </ul>
 );
