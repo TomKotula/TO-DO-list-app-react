68731ddbd54bdc70bee7c43f06776e6d7a66082e
Tom Kotula <test-git@youcode.pl>
2024-01-22 18:17:11 +0100

    toggleTaskDone interaction

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 import checkmark from './checkmark.png';
 import bin from './bin.png';
 
-const Tasks = ({ tasks, hideDone, removeTask }) => (
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
     <ul className="flex__list js-tasks">
         {tasks.map(task => (
             (hideDone && task.done) ? null : (
@@ -10,7 +10,7 @@ const Tasks = ({ tasks, hideDone, removeTask }) => (
                     key={task.id}
                     className="flex__listItem"
                 >
-                    <button className="js-done">
+                    <button className="js-done" onClick={() => toggleTaskDone(task.id)} >
                         {task.done ? <img src={checkmark} className="js-checkmarkIcon" alt="checkmark" /> : null}
                     </button>
                     <div className={`js-content ${task.done ? 'js-contentLineThrough' : ''}`}>
