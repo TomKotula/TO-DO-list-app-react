214f0775f16f6c583395d5154c676cf10003c467
Tom Kotula <test-git@youcode.pl>
2024-06-04 21:24:26 +0200

    focus added to input while clicking button

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import { FormFieldset, FormLabel, FormInput, AddButton } from "./styled";
+import useFocus from "./useFocus";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const [inputRef, setInputFocus] = useFocus();
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         addNewTask(newTaskContent.trim());
         setNewTaskContent("");
+        setInputFocus();
     };
 
     return (
@@ -17,6 +20,7 @@ const Form = ({ addNewTask }) => {
         </FormFieldset>
         <FormFieldset>
           <FormInput
+            ref={inputRef}
             value={newTaskContent}
             type="text"
             placeholder="What is there to do?"
