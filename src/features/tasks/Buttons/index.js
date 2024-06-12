28470af566cf4c9404b357cbf37847284b5e38cf
Tom Kotula <test-git@youcode.pl>
2024-06-12 20:53:02 +0200

    redux saga implemented and fetching example tasks is now available

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,26 +1,42 @@
-import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
+import {
+  selectTasks,
+  toggleHideDone,
+  setAllDone,
+  fetchExampleTasks,
+} from "../tasksSlice";
 import { ToggleButtonEvents, TextButtons } from "./styled";
 import { useSelector, useDispatch } from "react-redux";
 
 const Buttons = () => {
-  const {tasks, hideDone} = useSelector(selectTasks);
+  const { tasks, hideDone } = useSelector(selectTasks);
   const dispatch = useDispatch();
 
-  return tasks.length > 0 &&  (
-    <ToggleButtonEvents>
-      <TextButtons
-        onClick={() => dispatch(toggleHideDone())}
-      >
-        {hideDone ? "Show" : "Hide"} completed tasks
-      </TextButtons>
-      <TextButtons
-        disabled={tasks.every(({ done }) => done)}
-        onClick={() => dispatch(setAllDone())}
-      >
-        Complete all tasks
-      </TextButtons>
-    </ToggleButtonEvents>
+  return (
+    <div>
+      <ToggleButtonEvents>
+        <TextButtons
+          onClick={() => {
+            dispatch(fetchExampleTasks());
+          }}
+        >
+          Download example tasks
+        </TextButtons>
+        {tasks.length > 0 && (
+          <>
+            <TextButtons onClick={() => dispatch(toggleHideDone())}>
+              {hideDone ? "Show" : "Hide"} completed tasks
+            </TextButtons>
+            <TextButtons
+              disabled={tasks.every(({ done }) => done)}
+              onClick={() => dispatch(setAllDone())}
+            >
+              Complete all tasks
+            </TextButtons>
+          </>
+        )}
+      </ToggleButtonEvents>
+    </div>
   );
-}
+};
 
 export default Buttons;
--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -25,6 +25,10 @@ const tasksSlice = createSlice({
         task.done = true;
       });
     },
+    fetchExampleTasks: () => {},
+    setTasks: (state, { payload: tasks }) => {
+      state.tasks = tasks;
+    },
   },
 });
 
@@ -34,6 +38,8 @@ export const {
   toggleTaskDone,
   removeTask,
   setAllDone,
+  fetchExampleTasks,
+  setTasks,
 } = tasksSlice.actions;
 export const selectTasks = (state) => state.tasks;
 export default tasksSlice.reducer;
